fe2a0e2a2ecb092f62b94883fc40a2929ed3ae4d
Ishi Vashishtha <131875937+ishi1611@users.noreply.github.com>
2024-09-13 15:20:36 +0530

    Delete src directory

--- src/main/java/com/example/rentalcar/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.rentalcar.services;
-
-import com.example.rentalcar.entities.User;
-
-public interface UserService {
-    User saveUser(User user);
-    User findByUsername(String username);
-}
